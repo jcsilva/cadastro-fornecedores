3ec67e2a9670bd4e7d8b50dd8fa9dbcb252cbd56
Eduardo <jcsilva@pituba>
2019-05-10 09:02:43 -0300

    Troca configuração para usar bootstrap estático

--- config.py
+++ config.py
@@ -7,3 +7,6 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    BOOTSTRAP_SERVE_LOCAL = True # This turns file serving static
+                                 # https://pythonhosted.org/Flask-Bootstrap/configuration.html
+                                 # https://stackoverflow.com/questions/30185966/how-to-use-bootstrap-static-with-flask-bootstrap
